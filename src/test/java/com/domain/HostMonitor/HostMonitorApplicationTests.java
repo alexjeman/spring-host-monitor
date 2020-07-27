15c9a52cdad9851e1453156d3fe6686aa4c88a6e
alexjeman <contact@alexjeman.com>
2020-07-27 15:24:07 +0300

    Init Spring Web, PostgreSQL, JPA

---
+++ src/test/java/com/domain/HostMonitor/HostMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.HostMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
